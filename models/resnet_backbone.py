3c9303021044faa6503a16723535d11a7145c0d2
michaelb18 <michaelb18>
2025-04-09 22:04:09 -0400

    starter ResNet model w/ preprocessing, should combine this with text and metadata embedding. Or use CLIP if we want I saw that was in the jupyter notebook

---
+++ models/resnet_backbone.py
@@ -0,0 +1,22 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchmetrics
+from torchvision import models
+
+
+class ResNetBackBone(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        # init a pretrained resnet
+        backbone = models.resnet50(weights="DEFAULT")
+        num_filters = backbone.fc.in_features
+        layers = list(backbone.children())[:-1]
+        self.feature_extractor = nn.Sequential(*layers)
+        self.feature_extractor.eval()
+
+    def forward(self, x):
+        with torch.no_grad():
+            representations = self.feature_extractor(x).flatten(1)
+        return representations
