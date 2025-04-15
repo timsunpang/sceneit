2b374730f80ee1ecc01510ffb7d7b8f713ffc1a0
michaelb18 <michaelb18>
2025-04-14 21:02:56 -0400

    BERT preprocessing

--- models/resnet_backbone.py
+++ models/resnet_backbone.py
@@ -1,8 +1,8 @@
 import torch
 import torch.nn as nn
 from torchvision import models
-
-
+from dataloader import get_dataloader
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 class ResNetBackBone(nn.Module):
     def __init__(self):
         super().__init__()
@@ -16,5 +16,18 @@ class ResNetBackBone(nn.Module):
 
     def forward(self, x):
         with torch.no_grad():
+            x = x.to(device)
             representations = self.feature_extractor(x).flatten(1)
         return representations
+
+    def run_dataloader(self, dataloader):
+        with torch.no_grad():
+            for images, _ in dataloader:
+                representations = self(images).cpu().numpy()
+                yield representations
+
+if __name__ == "__main__":
+    resnet_backbone = ResNetBackBone().to("cuda")
+    dataloader = get_dataloader("../clean_data/downloaded_posters", batch_size=1, shuffle=False)
+    results = {"ResNetLatentVectors": [representations for representations in resnet_backbone.run_dataloader(dataloader)]}
+    print(results)
